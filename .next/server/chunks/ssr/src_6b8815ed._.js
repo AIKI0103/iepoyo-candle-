53697eb32660763acacfc382fdcac2164ff25f94
t-rm-0n <u3pom_6@icloud.com>
2025-11-09 17:34:22 +0900

    テスト

--- .next/server/chunks/ssr/src_6b8815ed._.js
+++ .next/server/chunks/ssr/src_6b8815ed._.js
@@ -4362,7 +4362,7 @@ const HeroSection = ()=>{
                             children: [
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("span", {
                                     className: "block",
-                                    children: "宮古島で、“きゅんん”とする想い出づくり💖"
+                                    children: "宮古島で、“きゅんん”とする想い出づくり"
                                 }, void 0, false, {
                                     fileName: "[project]/src/components/sections/hero-section.tsx",
                                     lineNumber: 25,
@@ -4401,7 +4401,7 @@ const HeroSection = ()=>{
                                     lineNumber: 39,
                                     columnNumber: 13
                                 }, this),
-                                "キラキラの海をぎゅっと閉じ込めた宝物みたいなキャンドルを手作り。",
+                                "キラキラの海をぎゅっと閉じ込めた宝物みたいなキャンドル。",
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("br", {
                                     className: "hidden sm:block"
                                 }, void 0, false, {
