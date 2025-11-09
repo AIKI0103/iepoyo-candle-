b387b2594a9ef396e1a357a52c745af117c59b09
AikiTanaka <aikitanaka@AikiTanakanoMacBook-Air.local>
2025-11-09 19:08:02 +0900

    topページの横幅レイアウト調整

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mkThtc+sD1riOGLOXT887u4eFXi5TwO4xGmIOpfKjIY=\"\n}"
--- .next/static/chunks/src_app_page_tsx_741c5d2b._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_bde8de61._.js",
-  "static/chunks/src_24e52b4f._.js",
-  "static/chunks/node_modules_64f64045._.js"
-],
-    source: "dynamic"
-});
