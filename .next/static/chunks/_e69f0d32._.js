002692588f6c22c50cd4b7b051be0ff89ac312ac
t-rm-0n <u3pom_6@icloud.com>
2025-11-16 13:57:34 +0900

    first commit

--- .next/static/chunks/_e69f0d32._.js
+++ .next/static/chunks/_e69f0d32._.js
@@ -1,10 +1,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_5679ca5a._.js",
-  "static/chunks/46a0d_next_dist_compiled_c488ed94._.js",
-  "static/chunks/46a0d_next_dist_client_8454df4e._.js",
-  "static/chunks/46a0d_next_dist_6c1267bd._.js",
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_eca9a946._.js",
+  "static/chunks/078ac_next_dist_compiled_afbc6318._.js",
+  "static/chunks/078ac_next_dist_client_17672085._.js",
+  "static/chunks/078ac_next_dist_6a9b15bf._.js",
   "static/chunks/61dca_@swc_helpers_cjs_e27e0aca._.js"
 ],
     source: "entry"
