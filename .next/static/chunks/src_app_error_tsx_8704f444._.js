2bdfd423a1682a077e142db48d49d6a3cfe48bbe
Aiki Tanaka <acine.photo.0103@gmail.com>
2025-11-23 00:48:28 +0900

    Remove build files and node_modules from git

--- .next/static/chunks/src_app_error_tsx_8704f444._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/8f20d_framer-motion_dist_es_index_mjs_6cd7789d._.js",
-  "static/chunks/node_modules__pnpm_4e696152._.js",
-  "static/chunks/src_7fda0864._.js"
-],
-    source: "dynamic"
-});
